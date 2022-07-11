cbd10984640ede9860bfc477b9b48a2e56087dff
yuto isogai <jandp.0717@gmail.com>
2022-07-11 22:02:01 +0900

    first commit

---
+++ src/hello.ts
@@ -0,0 +1,10 @@
+const greeter = (person: string): string => {
+    return `Hello, ${person}!`;
+  };
+  function testGreeter(): string {
+    const user = "Grant";
+    const msg = greeter(user);
+    Logger.log(msg);
+    return msg;
+  }
+  export { greeter, testGreeter };
